27dd35ff605424729516eedc75d0619626916ac7
Peter Sandor <peter@petersandor.name>
2016-07-09 22:31:30 +0200

    Bundle Bootstrap CSS

--- app/index.js
+++ app/index.js
@@ -4,4 +4,7 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var routes = require('./config/routes');
 
+// Bundle Bootstrap CSS
+require('bootstrap/dist/css/bootstrap.css');
+
 ReactDOM.render(routes, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,9 @@ module.exports = {
         }, {
             test: /\.css$/,
             loader: 'style-loader!css-loader'
+        },{
+            test   : /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+            loader : 'file-loader'
         }]
     },
     plugins: [
